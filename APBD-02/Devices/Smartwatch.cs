2766eab1e02886d42ef8006d045c5f525fe0fb39
setygur <131257541+setygur@users.noreply.github.com>
2025-03-19 15:15:01 +0100

    Update Smartwatch.cs

--- APBD-02/Devices/Smartwatch.cs
+++ APBD-02/Devices/Smartwatch.cs
@@ -1,6 +1,11 @@
 namespace APBD_02.Devices;
 
-public class Smartwatch
+public class Smartwatch : Device, IPowerNotifier
 {
+    private int _batteryPercentage;
+    public Smartwatch(int id, string name, bool isOn, int batteryPercentage) : base(id, name, isOn)
+    {
+        _batteryPercentage = batteryPercentage;
+    }
     
 }
