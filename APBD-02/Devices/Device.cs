00711b945f39111ec1f918e0631575eb83799dfb
setygur <131257541+setygur@users.noreply.github.com>
2025-03-19 15:11:46 +0100

    Update Device.cs

--- APBD-02/Devices/Device.cs
+++ APBD-02/Devices/Device.cs
@@ -1,6 +1,39 @@
 namespace APBD_02.Devices;
 
-public class Device
+public abstract class Device
 {
-    
+    private int _id;
+    private string _name;
+    private bool _isOn;
+
+    public Device(int id, string name, bool isOn)
+    {
+        _id = id;
+        _name = name;
+        _isOn = isOn;
+    }
+
+    public virtual void turnOn()
+    {
+        if (!_isOn)
+        {
+            _isOn = true;
+        }
+        else
+        {
+            Console.WriteLine("Device is already on");
+        }
+    }
+
+    public virtual void turnOff()
+    {
+        if (_isOn)
+        {
+            _isOn = false;
+        }
+        else
+        {
+            Console.WriteLine("Device is already off");
+        }
+    }
 }
