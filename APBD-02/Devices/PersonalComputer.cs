a2eda9eb07e0982d6af8e80f8201f2c2fd07c04e
setygur <131257541+setygur@users.noreply.github.com>
2025-03-26 14:22:24 +0100

    Refactored PersonalComputer.cs

--- APBD-02/Devices/PersonalComputer.cs
+++ APBD-02/Devices/PersonalComputer.cs
@@ -4,21 +4,18 @@ namespace APBD_02.Devices;
 
 public class PersonalComputer : Device
 {
-    private String? _operatingSystem;
+    public string? OperatingSystem { get; private set; }
 
-    public PersonalComputer(string id, string name, bool isOn) : base(id, name, isOn)
-    {
-        
-    }
+    public PersonalComputer(string id, string name, bool isOn) : base(id, name, isOn) {}
 
     public PersonalComputer(string id, string name, bool isOn, string? operatingSystem) : base(id, name, isOn)
     {
-        _operatingSystem = operatingSystem;
+        OperatingSystem = operatingSystem;
     }
 
     public override bool turnOn()
     {
-        if (_operatingSystem != null)
+        if (string.IsNullOrWhiteSpace(OperatingSystem))
         {
             return base.turnOn();
         }
@@ -27,6 +24,6 @@ public class PersonalComputer : Device
 
     public override string ToString()
     {
-        return Id + "," + Name + "," + IsOn + "," + _operatingSystem;
+        return Id + "," + Name + "," + IsOn + "," + OperatingSystem;
     }
 }
